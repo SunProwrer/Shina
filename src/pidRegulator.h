703f65520dbe15c498dadb6179a570893710c0ed
SunProwrer <89992175+SunProwrer@users.noreply.github.com>
2024-02-14 16:53:32 +0500

    Improved "#include" error (undefined reference to functions) by replace bodies of functions to .h file
    Исправлена ошибка "#include" (undefined reference to functions)
    путём переноса тел функций в .h файл
    
    Changed file:
    Изменённый файл:
        pidRegulator.h

--- src/pidRegulator.h
+++ src/pidRegulator.h
@@ -3,27 +3,78 @@
 
 class pidRegulator : public Regulator
 {
+public:
+    pidRegulator() : pidRegulator(1, 1, 1, 0.1) {}
+
+    pidRegulator(float kp, float ki, float kd, float dt) {
+        this->kp = kp;
+        this->ki = ki;
+        this->kd = kd;
+        this->dt = dt;
+
+        this->p = 0;
+        this->i = 0;
+        this->d = 0;
+    }
+
+    ~pidRegulator() {}
+
+    void setFactRPM(int16_t rpm) {
+        this->factRPM = rpm;
+    }
+
+    void setAimRPM(int16_t rpm) {
+        this->aimRPM = rpm;
+    }
+
+    void setP(float kp) {
+        this->kp = kp;
+    }
+
+    void setI(float ki) {
+        this->ki = ki;
+    }
+
+    void setD(float kd) {
+        this->kd = kd;
+    }
+
+    void setDt(float dt) {
+        this->dt = dt;
+    }
+
+    int16_t getControlVal() {
+        this->countPID();
+        return this->controlSignal;
+    }
+private:
+    void countErr() {
+        this->err = this->aimRPM - this->factRPM;
+    }
+    
+    void countP() {
+        this->countErr();
+        this->d = this->err;
+    }
+
+    void countI() {
+        this->i += err * dt;
+    }
+
+    void countD() {
+        static float prevErr = 0;
+        this->d = (err - prevErr) / dt;
+        prevErr = err;
+    }
+
+    void countPID() {
+        //TODO add map()
+        this->controlSignal = (int16_t)(this->p * this->kp + this->i * this->ki + this->d * this->kd);
+    }
 protected:
     float p, kp;
     float i, ki;
     float d, kd;
     float err;
     float dt;
-private:
-    void countErr();
-    void countP();
-    void countI();
-    void countD();
-    void countPID();
-public:
-    pidRegulator();
-    pidRegulator(float kp, float ki, float kd, float dt);
-    ~pidRegulator();
-    void setAimRPM(int16_t rpm);
-    void setFactRPM(int16_t rpm);
-    void setP(float kp);
-    void setI(float ki);
-    void setD(float kd);
-    void setDt(float dt);
-    int16_t getControlVal();
 };
