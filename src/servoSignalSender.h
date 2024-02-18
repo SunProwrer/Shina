dcd99a35cbca921654344517d095d8d99070e27d
SunProwrer <89992175+SunProwrer@users.noreply.github.com>
2024-02-18 23:21:40 +0500

    Added setters on min/max values in servoSignalSender.h
    Добавлены сеттеры на мин/макс значения в servoSignalSender.h

--- src/servoSignalSender.h
+++ src/servoSignalSender.h
@@ -3,21 +3,48 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-class servoRPMSetter : public MotorSignalSender {
+class servoSignalSender : public MotorSignalSender {
 public:
-    servoRPMSetter(Servo& servo) {
+    servoSignalSender(Servo& servo) {
         this->servo = &servo;
+
+        this->minMicroseconds = 800;
+        this->maxMicroseconds = 2300;
+        this->minInputVal = 0;
+        this->maxInputVal = 1000;
     }
+
+    ~servoSignalSender() { }
 
     void setControlVal(int16_t controlVal) {
         int16_t signal = convertToMicroseconds(controlVal);
         servo->writeMicroseconds(signal);
     }
-    
+
+    void setMinInputVal(int16_t minInputVal) {
+        this->minInputVal = minInputVal;
+    }
+
+    void setMaxInputVal(int16_t maxInputVal) {
+        this->maxInputVal = maxInputVal;
+    }
+
+    void setMinMicroseconds(int16_t minMicroseconds) {
+        this->minMicroseconds = minMicroseconds;
+    }
+
+    void setMaxMicroseconds(int16_t maxMicroseconds) {
+        this->maxMicroseconds = maxMicroseconds;
+    }
+
 protected:
     int16_t convertToMicroseconds(int16_t controlVal) {
-        return map(controlVal, 0, 1000, 800, 2300);
+        return map(controlVal, minInputVal, maxInputVal, minMicroseconds, maxMicroseconds);
     }
 
     Servo* servo;
+    int16_t minMicroseconds;
+    int16_t maxMicroseconds;
+    int16_t minInputVal;
+    int16_t maxInputVal;
 };
