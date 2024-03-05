77520425f83ce52d15a5a2b8743ea95cc516e16b
SunProwrer <89992175+SunProwrer@users.noreply.github.com>
2024-03-05 14:13:26 +0500

    Replased implementation to shina.h
    Перенесена реализация методов в shina.h

--- src/shina.h
+++ src/shina.h
@@ -9,14 +9,58 @@
 
 class Shina {
     public:
-        Shina();
-        ~Shina();
-        void tick();
-        void setRegulator(Regulator& regulator);
-        void setRPMCounter(RPMCounter& counter);
-        void setRPMSetter(RPMSetter& setter);
-        void setMotorSignalSender(MotorSignalSender& signalSender);
-        void setIOModule(IOModule& ioModule);
+        Shina() {
+            condition.aimRPM = 0;
+            condition.controlVal = 0;
+            condition.factRPM = 0;
+            condition.IOSettings = 0b00000000;
+            condition.libSettings = 0b00000000;
+        }
+
+        ~Shina() {
+            if (condition.libSettings & 1)
+            {
+                delete regulator;
+                delete counter;
+                delete setter;
+                delete signalSender;
+                delete ioModule;
+            }
+            
+        }
+
+        void tick() {
+            condition.factRPM = counter->getFactRPM();
+            condition.aimRPM = setter->getAimRPM();
+
+            regulator->setAimRPM(condition.aimRPM);
+            regulator->setFactRPM(condition.factRPM);
+            condition.controlVal = regulator->getControlVal();
+
+            signalSender->setControlVal(condition.controlVal);
+
+
+        }
+
+        void setRegulator(Regulator& regulator) {
+            this->regulator = &regulator;
+        }
+
+        void setRPMCounter(RPMCounter& counter) {
+            this->counter = &counter;
+        }
+
+        void setRPMSetter(RPMSetter& setter) {
+            this->setter = &setter;
+        }
+
+        void setMotorSignalSender(MotorSignalSender& signalSender) {
+            this->signalSender = &signalSender;
+        }
+
+        void setIOModule(IOModule& ioModule) {
+            this->ioModule = &ioModule;
+        }
     private:
         Condition condition;
         Regulator* regulator;
